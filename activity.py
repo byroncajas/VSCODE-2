41d5943ea994d5ca01ee086de261db54d850cde5
byron1996 <byroncajas151@hotmail.com>
2024-07-17 21:43:35 -0500

    commit

---
+++ activity.py
@@ -0,0 +1,13 @@
+"""ola"""
+my_variable="mi variable"
+my_other_variable="mi otra variable"
+my_int=1
+my_float=1.5
+my_bool=True
+my_bool=False
+my_string="ola mundo"
+my_other_string="python"
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
